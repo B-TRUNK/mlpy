3329f1f2504b113c2e64d031a2f20c567fc4932d
abanoyb_boshra <abanoub.boshra.anis.com>
2025-09-19 17:17:51 +0300

    vars_2

--- basics/var.py
+++ basics/var.py
@@ -5,3 +5,9 @@ spam ,Spam ,SPAM = 1 , 4 ,'spam!'
 print(spam ,Spam ,SPAM)
 
 #Mnemonic (Descriptive Variable Name)
+
+name    = 'Ahmed'
+space   = ' '
+age     = 35
+
+print(f"There was a boy named{space}{name} ,he was{space}{35} Years Old")
