06c4be6c65daa2b77b22dbdd2901d93d75ad1238
AbanoubBoushra <42608902+B-TRUNK@users.noreply.github.com>
2025-09-24 20:08:08 +0300

    dicts_2

--- DS/dicts.py
+++ DS/dicts.py
@@ -1,8 +1,13 @@
 dict = {
     "Jan" : "January",
     "Feb" : "February",  
-    "Mar" : "March"
+    "Mar" : "March",
+    4     : "April",
+    5     : ["May" ,"Mai" ,"Mayo" ,"Maggio"]
 }
 
 for key,value in dict:
     print(f"Key : {key},Value : {value}")
+
+print(dict["Feb"])
+print(dict.get("April" ,"Key Not Found"))
