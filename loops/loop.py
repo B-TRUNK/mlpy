1d0e0b5b99f748ce424926377c25c9937cd39faf
AbanoubBoushra <42608902+B-TRUNK@users.noreply.github.com>
2025-09-25 22:41:39 +0300

    Update loop.py

--- loops/loop.py
+++ loops/loop.py
@@ -1,8 +1,16 @@
 #While Loops
 
 i = 1
+
 while i <= 10:
-    print(i)
     i += 1
+    if 1 == 6:
+        continue
+    if i == 10:
+        break
+    print(i)
+    
+else :
+    print("Condition Fulfilled!")
 
 print("While Loop Has Ended!")
