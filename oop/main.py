f7346269fdc24b0e45b1c79ad53cab7c7f4205d3
AbanoubBoushra <42608902+B-TRUNK@users.noreply.github.com>
2025-10-01 21:03:38 +0300

    Update main.py

--- oop/main.py
+++ oop/main.py
@@ -1,5 +1,7 @@
 from Employee import Employee
 
-emp1 = Employee("Amr" ,35 ,"IT" ,False)
+emp1 = Employee("Amr" ,60 ,"IT" ,False ,5000)
+emp2 = Employee("Ahmed" ,45 ,"HR" ,False ,4000)
 
 emp1.print_info()
+emp1.bonus()
